f66b9b357422fafc77ae14e0e97ae09e854daf29
Animesh Rana <animesh.rana@hpe.com>
2020-05-21 17:02:05 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,131 @@
+################################################
+# Common
+################################################
+variable "friendly_name_prefix" {
+  type        = string
+  description = "String value for freindly name prefix for AWS resource names and tags"
+  default     = "dcbear"
+}
+
+variable "common_tags" {
+  type        = map(string)
+  description = "Map of common tags for taggable AWS resources"
+  default     = {}
+}
+
+################################################
+# Compute
+################################################
+variable "aws_ami" {
+  type        = string
+  description = "AWS ami id to create for Drupal app EC2 instances"
+  default     = "ami-0276cbf32e041c21b"
+}
+
+variable "instance_size" {
+  type        = string
+  description = "EC2 instance type for Drupal server"
+  default     = "t2.micro"
+}
+
+################################################
+# Network
+################################################
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID that Drupal app will be deployed into"
+  default     = "vpc-0ebb44d212c94dae4"
+}
+
+variable "route53_hosted_zone_name" {
+  type        = string
+  description = "Route53 Hosted Zone where Drupal Alias Record and Certificate Validation record will reside (required if tls_certificate_arn is left blank)"
+  default     = "nukdcbear.com"
+}
+
+################################################
+# Storage
+################################################
+variable "alb_subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs to use for Application Load Balancer (ALB)"
+  default     = ["subnet-0b44402b66af200ac", "subnet-00d706293e371e0d2"]
+}
+
+variable "ec2_subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs to use for EC2 instance - preferably private subnets"
+  default     = ["subnet-042cbf2b9863bdf4e", "subnet-0e30a970892d18361"]
+}
+
+variable "rds_subnet_ids" {
+  type        = list(string)
+  description = "Subnet IDs to use for RDS Database Subnet Group - preferably private subnets"
+  default     = ["subnet-042cbf2b9863bdf4e", "subnet-0e30a970892d18361"]
+}
+
+variable "rds_storage_capacity" {
+  type        = string
+  description = "Size capacity (GB) of RDS PostgreSQL database"
+  default     = 20
+}
+
+variable "rds_engine_version" {
+  type        = string
+  description = "Version of PostgreSQL for RDS engine"
+  default     = "11"
+}
+
+variable "rds_multi_az" {
+  type        = string
+  description = "Set to true to enable multiple availability zone RDS"
+  default     = "true"
+}
+
+variable "rds_instance_size" {
+  type        = string
+  description = "Instance size for RDS"
+  default     = "db.m4.large"
+}
+
+################################################
+# Security
+################################################
+variable "ingress_cidr_alb_allow" {
+  type        = list(string)
+  description = "List of CIDR ranges to allow web traffic ingress to Drupal Application Load Balancer (ALB)"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "ingress_cidr_ec2_allow" {
+  type        = list(string)
+  description = "List of CIDRs to allow SSH ingress to Drupal EC2 instance"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "tls_certificate_arn" {
+  type        = string
+  description = "ARN of ACM or IAM certificate to be used for Application Load Balancer HTTPS listeners (required if route53_hosted_zone_name is left blank)"
+  default     = ""
+}
+
+variable "kms_key_arn" {
+  type        = string
+  description = "ARN of KMS key to encrypt TFE S3 and RDS resources"
+  default     = ""
+}
+
+variable "ec2_ecs_ssh_key_pair" {
+  type        = string
+  description = "Name of SSH key pair for Drupal EC2 instance"
+  default     = "dcb-us-west-1"
+}
+
+################################################
+# App
+################################################
+variable "drupal_hostname" {
+  type        = string
+  description = "Hostname of Drupal instance"
+  default     = "myapp.nukdcbear.com"
+}
